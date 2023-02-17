c591470552c917fb47cb7b02b05396f097cc93de
Nadia <ivashchenko.nadia@gmail.com>
2023-02-17 17:32:03 +0200

    Initial Commit

---
+++ PH_RestorantMenu/PH_RestorantMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PH_RestorantMenuApp.swift
+//  PH_RestorantMenu
+//
+//  Created by Students on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PH_RestorantMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
